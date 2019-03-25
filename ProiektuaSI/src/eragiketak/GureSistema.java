7bc986a393a1422a7c79957c42b294dc5a5c24cf
ekaitzhara <-@10.109.84.33>
2019-03-25 20:34:39 +0100

    antzekotasun kalkulua eginda ;)

--- ProiektuaSI/src/eragiketak/GureSistema.java
+++ ProiektuaSI/src/eragiketak/GureSistema.java
@@ -8,10 +8,18 @@ import datuak.*;
 public class GureSistema {
 	// EMA klase orokorra
 	private static GureSistema nGureSistema=null;
+	
+	private HashMap<Integer, ArrayList<Float>> balorazioak;
+	private HashMap<Integer, String> izenburuak;
+	
 	private DatuenKarga karga;
+	private Antzekotasuna antzekotasun;
+	
 	
 	private GureSistema() {
 		karga = new KargaCSV();
+		balorazioak = new HashMap<Integer, ArrayList<Float>>();
+		izenburuak = new HashMap<Integer, String>();
 	}
 	
 	public static synchronized GureSistema getGureSistema() {
@@ -21,9 +29,12 @@ public class GureSistema {
 	}
 	
 	public HashMap<Integer, ArrayList<Float>> produktuenBalorazioak() {
-		return karga.produktuenBalorazioak();
+		this.balorazioak = karga.produktuenBalorazioak();
+		return this.balorazioak;
 	}
 	public HashMap<Integer, String> produktuenIzenburuak() {
-		return karga.produktuenIzenburuak();
+		this.izenburuak = karga.produktuenIzenburuak();
+		return this.izenburuak;
 	}
+	
 }
---
+++ ProiektuaSI/src/eragiketak/Kosinua.java
@@ -0,0 +1,59 @@
+package eragiketak;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Kosinua implements Antzekotasuna {
+	
+	public Kosinua() {
+		
+	}
+	
+	public Float antzekotasunaKalkulatu(int p1, int p2, HashMap<Integer, ArrayList<Float>> hashMap) {
+		ArrayList<Float> bal1 = hashMap.get(p1);
+		ArrayList<Float> bal2 = hashMap.get(p2);
+		Float emaitza, batukaria, num1, num2 = null;
+		Float batErro1, batErro2;
+		
+		batukaria= (float) 0.0;
+		batErro1= (float) 0.0;
+		batErro2 = (float) 0.0;
+		
+		if(p1 == p2) {
+			System.out.println("Zenbaki berdina sartu duzu!!!");
+			return (float) 0.0;
+		} else {	
+			while(!bal1.isEmpty() && !bal2.isEmpty()) {
+				num1 = bal1.remove(0);
+				num2 = bal2.remove(0);
+				
+				batukaria = batukaria + num1*num2;
+				
+				batErro1 = (float) (batErro1 + Math.pow(num1, 2));
+				batErro2 = (float) (batErro2 + Math.pow(num2, 2));
+				
+				System.out.println("batukaria: " + batukaria);
+				System.out.println("baterro1: " + batErro1);
+				System.out.println("baterro2: " + batErro2);
+			}
+			
+			Float erro1 = (float) Math.sqrt(batErro1);
+			Float erro2 = (float) Math.sqrt(batErro2);
+			System.out.println("erro1: " + erro1);
+			System.out.println("erro2: " + erro2);
+			
+			emaitza = batukaria / (erro1 * erro2);
+			System.out.println("emaitza: " + emaitza);
+			
+			return emaitza;
+		}	
+			
+	}
+	
+	public static void main(String[] args) {
+		Kosinua k = new Kosinua();
+		GureSistema n = GureSistema.getGureSistema();
+		k.antzekotasunaKalkulatu(808, 557, n.produktuenBalorazioak());
+	}
+
+}
