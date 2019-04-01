ddfa8d8e6e8278cc40be5a25d2158bfa1cc9f40b
ekaitzhara <-@10.109.84.33>
2019-04-01 20:44:12 +0200

    peta antzekotasunaKalkulatu

--- ProiektuaSI/src/eragiketak/Kosinua.java
+++ ProiektuaSI/src/eragiketak/Kosinua.java
@@ -20,30 +20,30 @@ public class Kosinua implements Antzekotasuna {
 		batErro2 = (float) 0.0;
 		
 		if(p1 == p2) {
-			System.out.println("Zenbaki berdina sartu duzu!!!");
+//			System.out.println("Zenbaki berdina sartu duzu!!!");
 			return (float) 0.0;
 		} else {	
 			while(!bal1.isEmpty() && !bal2.isEmpty()) {
 				num1 = bal1.remove(0);
 				num2 = bal2.remove(0);
 				
-				batukaria = batukaria + num1*num2;
+				batukaria = batukaria + num1 * num2;
 				
 				batErro1 = (float) (batErro1 + Math.pow(num1, 2));
 				batErro2 = (float) (batErro2 + Math.pow(num2, 2));
 				
-				System.out.println("batukaria: " + batukaria);
-				System.out.println("baterro1: " + batErro1);
-				System.out.println("baterro2: " + batErro2);
+//				System.out.println("batukaria: " + batukaria);
+//				System.out.println("baterro1: " + batErro1);
+//				System.out.println("baterro2: " + batErro2);
 			}
 			
 			Float erro1 = (float) Math.sqrt(batErro1);
 			Float erro2 = (float) Math.sqrt(batErro2);
-			System.out.println("erro1: " + erro1);
-			System.out.println("erro2: " + erro2);
+//			System.out.println("erro1: " + erro1);
+//			System.out.println("erro2: " + erro2);
 			
 			emaitza = batukaria / (erro1 * erro2);
-			System.out.println("emaitza: " + emaitza);
+//			System.out.println("emaitza: " + emaitza);
 			
 			return emaitza;
 		}	
