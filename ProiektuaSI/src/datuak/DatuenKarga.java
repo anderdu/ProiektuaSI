325ce88f066ea21a1e15d2e665ceda10501ab8d9
andur <andur@10.109.244.212>
2019-04-03 11:16:03 +0200

    prueba

--- ProiektuaSI/src/datuak/DatuenKarga.java
+++ ProiektuaSI/src/datuak/DatuenKarga.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 // Datuen karga egingo duen interfazea
-
+//para cargaCSV
 public interface DatuenKarga {
 
 	public HashMap<Integer, ArrayList<Float>> produktuenBalorazioak();
